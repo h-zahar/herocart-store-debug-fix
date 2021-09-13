f2fa774adddf7d4c3a1ec34052a590ca9b6bf452
iFall-iFail <64719808+iFall-iFail@users.noreply.github.com>
2021-09-13 15:16:49 +0600

    Small Fixes

--- js/app.js
+++ js/app.js
@@ -99,7 +99,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 
 loadProducts();
